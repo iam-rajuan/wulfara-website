c143908cb41d3af5bf02ea8f3725d5fa1945becb
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-07-03 01:49:21 +0600

    Refactor layout shell to render Navbar and Footer globally

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,15 +1,5 @@
-import Navbar from "@/components/shared/Navbar";
 import Faq from "@/components/faq/Faq";
-import Footer from "@/components/shared/Footer";
 
 export default function FaqPage() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Faq />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <Faq />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-import Navbar from "@/components/shared/Navbar";
 import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
 import Welcome from "@/components/home/Welcome";
-import Cta from "@/components/home/Cta";
-import Footer from "@/components/shared/Footer";
 import HomeFaq from "@/components/home/HomeFaq";
+import Cta from "@/components/home/Cta";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-        <Features />
-        <Welcome />
-        <HomeFaq />
-        <Cta />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <Features />
+      <Welcome />
+      <HomeFaq />
+      <Cta />
+    </>
   );
 }
